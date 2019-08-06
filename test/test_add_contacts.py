c08c1173dd87b0e5c4d85a13a24d4ee4853db934
Алина <alinasapaevam@gmail.com>
2019-08-06 21:15:02 +0300

    выполнение предусловий в тестах для удаления и модификации для групп и контактов

--- test/test_add_contacts.py
+++ test/test_add_contacts.py
@@ -5,13 +5,13 @@ from model.contact import Contact
 def test_add_new_contact(app):
     app.contact.add_new(Contact(firstname="Alina", middlename="Maratovna",lastname="Sapaeva", nickname="hhh",title="gsdhfg",
 company="ggygg", address="gyfhg", home="frrg", mobile="9030620645",  work="gfgdghyfyu", fax="ggfgf", email="gyfgf",   email2="gdtr",
-email3="nbjhg", homepage="ggfdghb",bday="12", bmonth="March", byear="1994", aday="17", amonth="May", ayear="2000", address2="gffgjf",
+email3="nbjhg", homepage="ggfdghb", bday="12", bmonth="March", byear="1994", aday="17", amonth="May", ayear="2000", address2="gffgjf",
 phone2 ="fygfsdf",  notes="hgk"))
 
 def test_add_empty_contact(app):
     app.contact.add_new(Contact(firstname="", middlename="",lastname="", nickname="",title="",
 company="", address="", home="", mobile="",  work="", fax="", email="",   email2="",
-email3="", homepage="",bday="-", bmonth="-", byear="-", aday="-", amonth="-", ayear="-", address2="",
+email3="", homepage="", bday="-", bmonth="-", byear="-", aday="-", amonth="-", ayear="-", address2="",
 phone2 ="",  notes=""))
 
 
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,2 +1,6 @@
+from model.contact import Contact
+
 def test_delete_first_contact(app):
+    if app.contact.count()==0:
+        app.contact.add_new(Contact(firstname="del", middlename="del"))
     app.contact.delete_first_contact()
