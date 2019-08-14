3cdd0b3efd697e70d2906a3a54b47b303a87e61a
Алина <alinasapaevam@gmail.com>
2019-08-14 21:59:45 +0300

    Ошибка 2

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,11 +1,12 @@
 from model.contact import Contact
 
 def test_delete_first_contact(app):
-    if app.contact.count()==0:
+    if app.contact.count() == 0:
         app.contact.add_new(Contact(firstname="del", middlename="del"))
-    old_contacts=app.contact.get_contact_list()
+    old_contacts=app.contact.get_contact_list
     app.contact.delete_first_contact()
-    new_contacts=app.contact.get_contact_list()
+    new_contacts=app.contact.get_contact_list
+    len(new_contacts)
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[0:1] = []
     assert old_contacts == new_contacts
