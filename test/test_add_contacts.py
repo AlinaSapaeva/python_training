b4552da9f9e5e3978b5b2f75fd0b3493e8efbc6b
Алина <alinasapaevam@gmail.com>
2019-09-09 14:59:40 +0300

    Задание №15: Параметризоваы тесты для создания групп и контактов Добавлена случайность при создании групп и контактов

--- test/test_add_contacts.py
+++ test/test_add_contacts.py
@@ -1,13 +1,37 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
+def random_string(prefix , maxlen):
+    simbols = string.ascii_letters + string.digits +  " "*4
+    return prefix + "".join([random.choice(simbols) for i in range(random.randrange(maxlen))])
 
-def test_add_new_contact(app):
+def random_number(maxlen):
+    symbols = string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="",
+                      company="", address="", homephone="", mobilephone="", workphone="", fax="", email="", email2="",
+                      email3="", homepage="",  address2="",
+                      phone2 ="", notes="")] + [
+    Contact(firstname=random_string("firstname", 5), middlename=random_string("middlename", 5),
+            lastname=random_string("lastname", 5), nickname=random_string("nickname", 5),
+            title=random_string("title", 5), company=random_string("company", 5),
+            address=random_string("address", 5), homephone=random_number(8),
+            mobilephone=random_number(8), workphone=random_number(8),
+            fax=random_string("fax", 5),email=random_string("email", 5),
+            email2=random_string("email2", 5), email3=random_string("email3", 5),
+            homepage=random_string("homepage", 5),
+            address2=random_string("address2", 10), phone2=random_string("phone2", 10),
+            notes=random_string("notes", 10),)
+    for i in range(2)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids = [repr(x) for x in testdata])
+def test_add_new_contact(app, contact):
     old_contacts=app.contact.get_contact_list()
-    contact=Contact(firstname="Alina", middlename="Maratovna", lastname="Sapaeva", nickname="hhh", title="gsdhfg",
-                    company="ggygg", address="gyfhg", homephone="frrg", mobilephone="9030620645", workphone="gfgdghyfyu", fax="ggfgf", email="gyfgf", email2="gdtr",
-                    email3="nbjhg", homepage="ggfdghb", bday="12", bmonth="March", byear="1994", aday="17", amonth="May", ayear="2000", address2="gffgjf",
-                    phone2 ="fygfsdf", notes="hgk")
     app.contact.add_new(contact)
     # хеширование      == app.contact.count()
     assert len(old_contacts) + 1 == app.contact.count()
@@ -15,17 +39,6 @@ def test_add_new_contact(app):
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-def test_add_empty_contact(app):
-    old_contacts=app.contact.get_contact_list()
-    contact = Contact(firstname="", middlename="", lastname="", nickname="", title="",
-                      company="", address="", homephone="", mobilephone="", workphone="", fax="", email="", email2="",
-                      email3="", homepage="", bday="-", bmonth="-", byear="-", aday="-", amonth="-", ayear="-", address2="",
-                      phone2 ="", notes="")
-    app.contact.add_new(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts=app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
