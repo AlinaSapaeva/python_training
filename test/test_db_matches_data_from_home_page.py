6d39ce28815a4d6767d3758dbea48b59d783a15c
Алина <alinasapaevam@gmail.com>
2019-09-12 19:46:07 +0300

    21.  Переделать тесты для проверки информации о контактах на главной странице

--- test/test_db_matches_data_from_home_page.py
+++ test/test_db_matches_data_from_home_page.py
@@ -11,3 +11,5 @@ def test_db_matches_data_from_home_page_use_orm(app, db):
     all_contacts_from_ui = app.contact.get_contact_list()
     all_contacts_from_db = db.get_contact_list()
     assert sorted(all_contacts_from_ui, key=Contact.id_or_max) == sorted(all_contacts_from_db, key=Contact.id_or_max)
+
+
